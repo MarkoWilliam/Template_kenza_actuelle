eddc7e88f99bc8c206ae3f8ab69b4378264d4ab7
MarkoWilliam <yowanwillim@gmail.com>
2020-11-20 13:05:25 +0300

    Filter ok

--- src/app/main/produits/produits.component.ts
+++ src/app/main/produits/produits.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';  
 import { GlobaleService } from '../service/globale.service';
@@ -13,28 +14,7 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'}, 
-];
+
 
 @Component({
   selector: 'app-produits',
@@ -46,10 +26,10 @@ export class ProduitsComponent
   listProduit: MatTableDataSource<any>;
  
   displayedColumns: string[] = ['position', 'nom', 'couleur', 'stock_dispo', 'statue', 'prix', 'prix_solde', 'actions'];
- 
+  searchKey: string;
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-
+  @ViewChild(MatPaginator,  {static: true}, ) paginator: MatPaginator;
+  @ViewChild(MatSort,  {static: true}, ) sort: MatSort;
   constructor(public servGlobal : GlobaleService) {}
   ngOnInit() {
     this.ListeProduit();
@@ -59,12 +39,22 @@ export class ProduitsComponent
 ListeProduit() {
   this.servGlobal.listeProduit().subscribe(results => {
    this.listProduit = new MatTableDataSource(results.body);
+   this.listProduit.sort = this.sort;
    this.listProduit.paginator = this.paginator;
    console.log("list produit", this.listProduit);
    console.log("list produit pagination",    this.listProduit.paginator);
   })
 }
  
+
+applyFilter() {
+  this.listProduit.filter =this.searchKey.trim().toLowerCase();
+}
+
+onSearchClear() {
+  this.searchKey="";
+  this.applyFilter();
+}
 }
 
 
--- src/app/main/produits/produits.module.ts
+++ src/app/main/produits/produits.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthGuard } from '../service/auth/auth.guard';
 import { ProduitsComponent } from './produits.component';
-import { MatCheckboxModule, MatIconModule, MatTableModule } from '@angular/material' 
+import { MatCheckboxModule, MatInputModule, MatIconModule, MatSortModule, MatTableModule } from '@angular/material' 
 import { MatPaginatorModule } from '@angular/material';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 const routes = [
   {
       path     : 'produits',
@@ -22,15 +23,24 @@ const routes = [
       MatTableModule,
       MatPaginatorModule ,
       MatIconModule,
-      MatCheckboxModule
-
+      MatCheckboxModule,
+      MatSortModule,
+      FormsModule,
+      ReactiveFormsModule,
+      MatInputModule,
+      BrowserModule    
   ],
   exports     : [
     ProduitsComponent,
     MatTableModule,
     MatPaginatorModule,
     MatIconModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSortModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatInputModule,
+    BrowserModule     
   ]
 })
 
